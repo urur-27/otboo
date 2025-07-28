ef6a31664aad1e3b7ab6409460b6a1eaed72a5e9
kitmtaewoo <bezzi1654@kookmin.ac.kr>
2025-07-28 17:08:56 +0900

    feat: follow entity 정의

---
+++ src/main/java/com/team3/otboo/domain/follow/entity/Follow.java
@@ -0,0 +1,48 @@
+package com.team3.otboo.domain.follow.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import java.util.UUID;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+@Table(name = "follows")
+@Entity
+@Getter
+@ToString
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Follow {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.UUID)
+	private UUID id;
+
+	@CreatedDate
+	private LocalDateTime createdAt;
+
+	@LastModifiedDate
+	private LocalDateTime updatedAt;
+
+	@Column(nullable =false)
+	private UUID followeeId;
+
+	@Column(nullable = false)
+	private UUID followerId;
+
+	public static Follow create(UUID followeeId, UUID followerId){
+		Follow follow = new Follow();
+		follow.followeeId = followeeId;
+		follow.followerId = followerId;
+
+		return follow;
+	}
+}
