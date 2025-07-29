bcec18fb56f822b42b9897aa9a69679433405032
kitmtaewoo <bezzi1654@kookmin.ac.kr>
2025-07-29 10:07:59 +0900

    feat: follow dto, controller, service, repository 뼈대 코드 기능 구현

---
+++ src/main/java/com/team3/otboo/domain/follow/service/response/FollowListResponse.java
@@ -0,0 +1,18 @@
+package com.team3.otboo.domain.follow.service.response;
+
+import com.team3.otboo.domain.follow.dto.FollowDto;
+import java.util.List;
+import java.util.UUID;
+import org.hibernate.query.SortDirection;
+
+public record FollowListResponse(
+	List<FollowDto> data,
+	String nextCursor,
+	UUID nextIdAfter,
+	boolean hasNext,
+	int totalCount,
+	String sortBy,
+	SortDirection sortDirection
+) {
+
+}
