fb15dfc0161600f17b7699e2cc7e2f4e9bcfe683
kitmtaewoo <bezzi1654@kookmin.ac.kr>
2025-07-30 13:20:31 +0900

    refactor: 애노테이션 수정

--- src/main/java/com/team3/otboo/domain/follow/entity/UserFollowingCount.java
+++ src/main/java/com/team3/otboo/domain/follow/entity/UserFollowingCount.java
@@ -1,10 +1,19 @@
 package com.team3.otboo.domain.follow.entity;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import java.util.UUID;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Table(name = "user_following_count")
+@Entity
+@Getter
+@ToString
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class UserFollowingCount {
 
 	@Id
