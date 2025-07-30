360df8f38a27a7032866c247e149afabe8e12229
kitmtaewoo <bezzi1654@kookmin.ac.kr>
2025-07-30 18:47:19 +0900

    feat: feed repository 기능 구현

--- src/main/java/com/team3/otboo/domain/feed/repository/FeedRepository.java
+++ src/main/java/com/team3/otboo/domain/feed/repository/FeedRepository.java
@@ -1,4 +1,11 @@
 package com.team3.otboo.domain.feed.repository;
 
-public interface FeedRepository {
+import com.team3.otboo.domain.feed.entity.Feed;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FeedRepository extends JpaRepository<Feed, UUID> {
+
 }
