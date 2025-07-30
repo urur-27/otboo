8150535dd39aafb9a1f1b8621b0db003cf341679
kitmtaewoo <bezzi1654@kookmin.ac.kr>
2025-07-30 18:01:21 +0900

    feat: 댓글 api (피드 댓글 조회, 피드 댓글 등록, 피드 댓글 삭제) 기능 구현

---
+++ src/main/java/com/team3/otboo/domain/feed/mapper/AuthorMapper.java
@@ -0,0 +1,17 @@
+package com.team3.otboo.domain.feed.mapper;
+
+import com.team3.otboo.domain.feed.dto.AuthorDto;
+import com.team3.otboo.domain.user.entity.User;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuthorMapper {
+
+	public AuthorDto toDto(User user) {
+		return new AuthorDto(
+			user.getId(),
+			user.getUsername(),
+			user.getProfileImageUrl()
+		);
+	}
+}
