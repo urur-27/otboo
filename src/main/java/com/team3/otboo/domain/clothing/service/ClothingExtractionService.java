5f975e76d810200cb6ea548ab117fc5f8bb05fa2
김민식 <u1r2r3@naver.com>
2025-07-31 18:56:47 +0900

    작업내용 중간 저장

---
+++ src/main/java/com/team3/otboo/domain/clothing/service/ClothingExtractionService.java
@@ -0,0 +1,10 @@
+package com.team3.otboo.domain.clothing.service;
+
+import com.team3.otboo.domain.clothing.dto.ClothingDto;
+
+public interface ClothingExtractionService {
+
+    ClothingDto extractAndSave(String url);
+
+
+}
---
+++ src/main/java/com/team3/otboo/domain/clothing/service/ClothingExtractionServiceImpl.java
@@ -0,0 +1,15 @@
+package com.team3.otboo.domain.clothing.service;
+
+import com.team3.otboo.domain.clothing.dto.ClothingDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ClothingExtractionServiceImpl implements ClothingExtractionService {
+
+    @Override
+    public ClothingDto extractAndSave(String url) {
+        return null;
+    }
+}
