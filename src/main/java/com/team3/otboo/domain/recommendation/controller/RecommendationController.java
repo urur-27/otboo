d91eeb0cb6ba1d215e16bf0cbe9755fc3b73d716
쿼카좋아 <a939515@gmail.com>
2025-08-04 18:13:49 +0900

    feat: 의상 추천 앤드포인트 구현 JPaAuditing 설정 분리

--- src/main/java/com/team3/otboo/domain/recommendation/controller/RecommendationController.java
+++ src/main/java/com/team3/otboo/domain/recommendation/controller/RecommendationController.java
@@ -1,5 +1,26 @@
 package com.team3.otboo.domain.recommendation.controller;
 
+import com.team3.otboo.domain.recommendation.dto.RecommendationDto;
+import com.team3.otboo.domain.recommendation.service.RecommendationService;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/recommendations")
+@RequiredArgsConstructor
 public class RecommendationController {
 
+  private final RecommendationService recommendationService;
+
+  @GetMapping
+  public ResponseEntity<RecommendationDto> getRecommendation(@RequestParam UUID userId) {
+    RecommendationDto recommendation = recommendationService.recommend(userId);
+
+    return ResponseEntity.ok(recommendation);
+  }
 }
---
+++ src/test/java/com/team3/otboo/domain/recommendation/controller/RecommendationControllerTest.java
@@ -0,0 +1,46 @@
+package com.team3.otboo.domain.recommendation.controller;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+import com.team3.otboo.domain.recommendation.dto.RecommendationDto;
+import com.team3.otboo.domain.recommendation.service.RecommendationService;
+import java.util.List;
+import java.util.UUID;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(RecommendationController.class)
+public class RecommendationControllerTest {
+
+  @Autowired
+  private MockMvc mockMvc;
+
+  @MockitoBean
+  private RecommendationService recommendationService;
+
+  @Test
+  @WithMockUser
+  @DisplayName("의상 추천 API를 호출하면, 성공(200 OK) 상태와 함계 추천 DTO를 반환한다")
+  void getRecommendation_Success() throws Exception {
+    // given:
+    UUID userId = UUID.randomUUID();
+    RecommendationDto expectedReuslt = new RecommendationDto(UUID.randomUUID(), userId, List.of());
+
+    when(recommendationService.recommend(userId)).thenReturn(expectedReuslt);
+
+    mockMvc.perform(get("/api/recommendations")
+            .param("userId", userId.toString()))
+        .andExpect(status().isOk())
+        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+        .andExpect(jsonPath("$.userId").value(userId.toString()));
+  }
+}
