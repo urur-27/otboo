37e8ea91e9fffe62b29c3b65565a4f54f473de88
kitmtaewoo <bezzi1654@kookmin.ac.kr>
2025-08-14 16:20:52 +0900

    feat: 조회수 서비스 구현

---
+++ src/main/java/com/team3/otboo/domain/feed/service/ViewService.java
@@ -0,0 +1,28 @@
+package com.team3.otboo.domain.feed.service;
+
+import com.team3.otboo.domain.feed.repository.FeedViewCountRepository;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ViewService {
+
+	private final FeedViewCountRepository feedViewCountRepository;
+	private final ViewCountBackUpProcessor viewCountBackUpProcessor;
+	
+	private static final int BACK_UP_BACH_SIZE = 100;
+
+	public Long increase(UUID feedId) {
+		Long count = feedViewCountRepository.increase(feedId);
+		if (count % BACK_UP_BACH_SIZE == 0) { // 100 단위마다 데이터베이스에 백업
+			viewCountBackUpProcessor.backUp(feedId, count);
+		}
+		return count;
+	}
+
+	public Long count(UUID feedId) {
+		return feedViewCountRepository.read(feedId);
+	}
+}
