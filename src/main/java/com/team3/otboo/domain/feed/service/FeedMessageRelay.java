ec37796e434ce7775f5ce14528db0285d4f489a6
kitmtaewoo <bezzi1654@kookmin.ac.kr>
2025-08-21 17:22:18 +0900

    refactor: Message Relay 위치 수정

--- src/main/java/com/team3/otboo/domain/feed/service/FeedMessageRelay.java
+++ src/main/java/com/team3/otboo/domain/feed/service/FeedMessageRelay.java
@@ -1,16 +1,15 @@
-package com.team3.otboo.domain.dm.service;
+package com.team3.otboo.domain.feed.service;
 
-import com.team3.otboo.common.dataSerializer.DataSerializer;
 import com.team3.otboo.common.outboxMessageRelay.Outbox;
 import com.team3.otboo.common.outboxMessageRelay.OutboxEvent;
 import com.team3.otboo.common.outboxMessageRelay.OutboxRepository;
-import com.team3.otboo.domain.dm.event.payload.DirectMessageSentPayload;
 import java.time.Instant;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Pageable;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -20,12 +19,14 @@ import org.springframework.transaction.event.TransactionalEventListener;
 @Component
 @RequiredArgsConstructor
 @Slf4j
-public class DMMessageRelay {
+public class FeedMessageRelay {
 
 	private final OutboxRepository outboxRepository; // outbox repository 에 서 주기적으로 미전송 데이터 가져옴
+	private final KafkaTemplate<String, String> kafkaTemplate;
 
-	private final PublishService publishService;
+	// commit 직전에 outbox 를 생성하고 . commit 직후에 message relay 에게 바로 OutboxEvent 전달.
 
+	// spring 은 AOP 프록시를 통해 @Transactional 의 전후를 감시함 (이벤트를 발행한 쪽의 트랜잭션 상태에 자신을 맞춤)
 	// 이벤트를 발행시킨 작업의 트랜잭션이 특정 상태가 될때까지 기다렸다가 실행됨 .
 	@TransactionalEventListener(phase = TransactionPhase.BEFORE_COMMIT)
 	public void createOutbox(OutboxEvent outboxEvent) {
@@ -40,14 +41,12 @@ public class DMMessageRelay {
 
 	private void publishEvent(Outbox outbox) {
 		try {
-			// outbox 에서 payload 꺼내서 DirectMessageSendPayload 로 바꿔서 redis template 으로 전달 .
-			DirectMessageSentPayload payload = DataSerializer.deserialize(
-				outbox.getPayload(),
-				DirectMessageSentPayload.class
-			);
+			kafkaTemplate.send(
+				outbox.getEventType().getTopic(),
+				outbox.getPayload()
+			).get(1, TimeUnit.SECONDS); // 1초 동안 전송 완료 메시지를 기다림 .
 
-			publishService.publish(payload);
-			outboxRepository.delete(outbox);
+			outboxRepository.delete(outbox); // 여기서 충돌 발생..
 		} catch (Exception e) {
 			log.error("[MessageRelay.publishEvent] outbox={}", outbox, e);
 		}
@@ -64,7 +63,7 @@ public class DMMessageRelay {
 				Instant.now().minusSeconds(10), // 생성된지 10초가 지난 메시지 가져옴 .
 				Pageable.ofSize(100)
 			);
-//		log.info("[MessageRelay] Polling outbox messages. size={}", outboxes.size());
+//		log.info("[FeedMessageRelay] Polling outbox messages. size={}", outboxes.size());
 
 		for (Outbox outbox : outboxes) {
 			publish(OutboxEvent.of(outbox));
