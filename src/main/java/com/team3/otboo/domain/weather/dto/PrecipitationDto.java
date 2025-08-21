4f9c393abb4ced5f6be2d8c903143d14254642c1
kitmtaewoo <bezzi1654@kookmin.ac.kr>
2025-08-21 13:43:57 +0900

    refactor: 오류 수정

--- src/main/java/com/team3/otboo/domain/weather/dto/PrecipitationDto.java
+++ src/main/java/com/team3/otboo/domain/weather/dto/PrecipitationDto.java
@@ -4,22 +4,26 @@ import com.team3.otboo.domain.weather.entity.Precipitation;
 import com.team3.otboo.domain.weather.enums.PrecipitationType;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
+@NoArgsConstructor
 public class PrecipitationDto {
-    private PrecipitationType type;
-    private Double amount;
-    private Double probability;
 
-    @Builder
-    private PrecipitationDto(PrecipitationType type, Double amount, Double probability) {
-        this.type = type;
-        this.amount = amount;
-        this.probability = probability;
-    }
+	private PrecipitationType type;
+	private Double amount;
+	private Double probability;
 
-    public static PrecipitationDto create(Precipitation precipitation) {
-        return new PrecipitationDto(precipitation.getType(), precipitation.getAmount(), precipitation.getProbability());
-    }
+	@Builder
+	private PrecipitationDto(PrecipitationType type, Double amount, Double probability) {
+		this.type = type;
+		this.amount = amount;
+		this.probability = probability;
+	}
+
+	public static PrecipitationDto create(Precipitation precipitation) {
+		return new PrecipitationDto(precipitation.getType(), precipitation.getAmount(),
+			precipitation.getProbability());
+	}
 }
