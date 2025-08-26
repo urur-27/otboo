1d41a0558588cfbcbdc92b002865e61797fdc1ed
김민식 <u1r2r3@naver.com>
2025-08-26 14:10:01 +0900

    chore: 불필요한 로직 제거

--- src/main/java/com/team3/otboo/domain/clothing/infrastructure/OpenAiVisionClient.java
+++
@@ -1,5 +0,0 @@
-package com.team3.otboo.domain.clothing.infrastructure;
-
-public interface OpenAiVisionClient {
-    String sendImagePrompt(String imageUrl, String systemPrompt, String userPrompt);
-}
